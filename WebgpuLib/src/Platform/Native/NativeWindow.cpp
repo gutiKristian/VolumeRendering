c7767250dc3b192003721235aeab6581b5c4d04f
Kristian Gutic <kristian.gutic@gmail.com>
2023-12-20 21:11:46 +0100

    Using emplace

--- WebgpuLib/src/Platform/Native/NativeWindow.cpp
+++ WebgpuLib/src/Platform/Native/NativeWindow.cpp
@@ -65,7 +65,7 @@ namespace base {
 	void Window::OnWindowClosed(GLFWwindow* windowHandle)
 	{
 		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
-		window->m_Events.push_back(Event(WindowClosedEvent{}));
+		window->m_Events.emplace_back(WindowClosedEvent{});
 	}
 
 	void Window::OnWindowResized(GLFWwindow* windowHandle, int width, int height)
@@ -73,7 +73,7 @@ namespace base {
 		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
 		window->m_Width = static_cast<uint32_t>(width);
 		window->m_Height = static_cast<uint32_t>(height);
-		window->m_Events.push_back(Event(WindowResizedEvent{ .width = static_cast<uint32_t>(width), .height = static_cast<uint32_t>(height) }));
+		window->m_Events.emplace_back(WindowResizedEvent{ .width = static_cast<uint32_t>(width), .height = static_cast<uint32_t>(height) });
 	}
 
 	void Window::OnKeyPressed(GLFWwindow* windowHandle, int key, int scancode, int action, int mods)
@@ -82,11 +82,11 @@ namespace base {
 
 		if (action == GLFW_RELEASE)
 		{
-			window->m_Events.push_back(Event(KeyReleasedEvent{ .key = key }));
+			window->m_Events.emplace_back(KeyReleasedEvent{ .key = key });
 		}
 		else
 		{
-			window->m_Events.push_back(Event(KeyPressedEvent{ .key = key, .repeat = action == GLFW_PRESS }));
+			window->m_Events.emplace_back(KeyPressedEvent{ .key = key, .repeat = action == GLFW_PRESS });
 		}
 	}
 
@@ -96,24 +96,24 @@ namespace base {
 
 		if (action == GLFW_RELEASE)
 		{
-			window->m_Events.push_back(Event(MouseReleasedEvent{ .button = button }));
+			window->m_Events.emplace_back(MouseReleasedEvent{ .button = button });
 		}
 		else
 		{
-			window->m_Events.push_back(Event(MousePressedEvent{ .button = button, .repeat = action == GLFW_PRESS }));
+			window->m_Events.emplace_back(MousePressedEvent{ .button = button, .repeat = action == GLFW_PRESS });
 		}
 	}
 
 	void Window::OnMouseScrolled(GLFWwindow* windowHandle, double xoffset, double yoffset)
 	{
 		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
-		window->m_Events.push_back(Event(MouseScrolledEvent{ .xOffset = static_cast<float>(xoffset), .yOffset = static_cast<float>(yoffset) }));
+		window->m_Events.emplace_back(MouseScrolledEvent{ .xOffset = static_cast<float>(xoffset), .yOffset = static_cast<float>(yoffset) });
 	}
 
 	void Window::OnMouseMoved(GLFWwindow* windowHandle, double xpos, double ypos)
 	{
 		Window* window = static_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
-		window->m_Events.push_back(Event(MouseMovedEvent{ .xPos = static_cast<float>(xpos), .yPos = static_cast<float>(ypos) }));
+		window->m_Events.emplace_back(MouseMovedEvent{ .xPos = static_cast<float>(xpos), .yPos = static_cast<float>(ypos) });
 	}
 
 }
