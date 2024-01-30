a50383069cd8a353f89d9f721d0e616ed12aa339
Kristian Gutic <kristian.gutic@gmail.com>
2024-01-30 16:51:45 +0100

    Disabling normalizing of data

--- App/src/dicom/VolumeFile.h
+++ App/src/dicom/VolumeFile.h
@@ -66,7 +66,7 @@ namespace med
 
 			for (std::uint32_t i = 0; i < size; ++i)
 			{
-				fData[i] = static_cast<float>(data[i]) / 4095.0f;
+				fData[i] = static_cast<float>(data[i]);
 			}
 			p_DataFloat = std::shared_ptr<void>(fData);
 		}
